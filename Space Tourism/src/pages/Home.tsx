287cb5ae67f6691aaeb4cdc6bdbd42afdfedfb3b
Anas Moner <ansmoner2000@gmail.com>
2024-11-26 11:46:53 +0200

    add assets and change favicon

--- Space Tourism/src/pages/Home.tsx
+++ Space Tourism/src/pages/Home.tsx
@@ -1,9 +1,16 @@
 
+/**
+ * CSS Links
+ */
+import '../index.css';
+
+
 const Home = () => {
   return (
-    <div className="home">
-    <header>
-        <div>
+    <div className="home w-full h-screen">
+    <header className="flex justify-between items-center">
+        <img src='../assets/shared/logo.svg' alt="logo" />
+        <div className="flex gap-4">
             <p>00 Home</p>
             <p>01 Destination</p>
             <p>02 Crew</p>
@@ -13,7 +20,7 @@ const Home = () => {
     <main>
         <p>
             So, you want to travel to
-            Space
+            <span className="">Space</span>  
             Let&apos;s face it; if you want to go to space, you might as well genuinely go to 
             outer space and not hover kind of on the edge of it. Well sit back, and relax 
             because we&apos;ll give you a truly out of this world experience!
